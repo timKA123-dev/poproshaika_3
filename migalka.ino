22cddf29df1996235f63b1240b0b0ef0e29deb98
timKA123-dev <timurskiy333@gmail.com>
2026-06-03 08:36:04 +0400

    Add files via upload

---
+++ migalka.ino
@@ -0,0 +1,23 @@
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+
+  digitalWrite(led3, HIGH);
+  delay(500);
+  digitalWrite(led3, LOW);
+}
