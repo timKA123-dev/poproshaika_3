324d73d618b34ad9fcf78b55da3de82c3d2d0318
timKA123-dev <timurskiy333@gmail.com>
2026-06-03 08:28:04 +0400

    Add files via upload

---
+++ HELLOWORLD.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // Starts serial communication at 9600 bits per second
+  Serial.begin(9600); 
+  
+  // Prints the message to the Serial Monitor
+  Serial.println("Hello World!");
+}
+
+void loop() {
+  // Stay empty if you only want to print once
+}
