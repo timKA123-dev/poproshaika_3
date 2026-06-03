17c08c9db36db5fa8b57aa19f85a48767a1bf924
timKA123-dev <timurskiy333@gmail.com>
2026-06-03 08:37:09 +0400

    Add files via upload

---
+++ привет мир.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.println("Привет, мир!");
+  display.display();
+}
+
+void loop() {}
